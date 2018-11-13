6998861905e23b120cc301398568efe2d73ca8a7
JeromeMouly <mouly.jerome@gmail.com>
2018-11-13 16:28:40 +0100

    add modal cotisation membre

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,18 @@ function myFunction() {
 }
 
 function closeOnClick() {
-       let nav =  document.getElementById("navBar");
-       nav.className = nav.className.replace(" w3-show", "");
+    let nav = document.getElementById("navBar");
+    nav.className = nav.className.replace(" w3-show", "");
 
+}
+
+/* ----- DROPDOWNS MODAL BECOME MEMBERS ----- */
+
+function myFunction() {
+    var x = document.getElementById("chooseNumbers");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
 }
